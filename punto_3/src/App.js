cb9719f5c81dc192ca374812e877729c418266ee
Exequiel Vega <lfexevega95@gmail.com>
2025-08-26 01:15:37 -0300

    fix

--- punto_3/src/App.js
+++ punto_3/src/App.js
@@ -1,17 +1,17 @@
+// src/App.jsx (solo ejemplo; no es obligatorio tocarlo)
 import "./App.css";
 import BarraBusqueda from "./components/barraBusqueda/barraBusqueda";
 import NavbarMarvel from "./components/navbarMarvel/navbarMarvel";
 
 function App() {
-  console.log(process.env.REACT_APP_API_KEY);
+  console.log("PUBLIC:", process.env.REACT_APP_MARVEL_PUBLIC_KEY);
   return (
     <div className="colorAf">
       <div className="App centre">
-        <NavbarMarvel></NavbarMarvel>
-        <BarraBusqueda></BarraBusqueda>
+        <NavbarMarvel />
+        <BarraBusqueda />
       </div>
     </div>
   );
 }
-
 export default App;
